820d050f493f15f378143f821f38f42ef6a08e02
danielwhitford <whitford.dsw@gmail.com>
2021-10-14 23:32:59 +0100

    Delete file

--- src/main/java/Hufdan.java
+++ src/main/java/Hufdan.java
@@ -9,19 +9,15 @@ import java.nio.file.Path;
 
 public class Hufdan {
     public static void main(String... args) throws IOException, ClassNotFoundException {
-        var s = Files.readString(Path.of("/Users/danielwhitford/workspace/Hufdan/src/main/java/encode/HufdanEncoder.java"));
-//        var s = "This is a long message";
+        var s = Files.readString(Path.of("test.in"));
 
         var e = HufdanEncoder.encode(s);
-        System.out.println("Original: " + s);
 
         var writer = new HufdanFileWriter(e);
         writer.write("test.out");
 
         var ee = HufdanFileReader.read("test.out");
         String decoded = HufdanDecoder.decode(ee);
-        System.out.println("Decoded: " + decoded);
-
         assert (s.equals(decoded));
     }
 }
